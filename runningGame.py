b005cab23777aaab323f9b00bd840bb1696e8006
toastyho <57785244+toastyho@users.noreply.github.com>
2022-04-20 10:59:56 -0600

    added working timer and 'game over' state

--- runningGame.py
+++ runningGame.py
@@ -6,19 +6,47 @@ DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 400
 running = True
 
-# display screen settings
+# window and frame settings
 pygame.init() # initialize the pygame module (like starting a car with key)
 screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT)) # display size
 pygame.display.set_caption('RunningToast')
 timer = pygame.time.Clock()
 bgTextStyle = pygame.font.Font('Grand9K Pixel.ttf', 50)
 
+# 'playing' state background settings
 background =  pygame.image.load('Graphics/kitchen_BG.jpg').convert()
 background = pygame.transform.scale(background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
 bgX = 0
 bgX2 = DISPLAY_WIDTH
-bgText = bgTextStyle.render('Run, ma boi', False, 'white')
-bgText_rect = bgText.get_rect(center=(400, 50))
+
+timeTextStyle = pygame.font.Font('Grand9k Pixel.ttf', 20)
+start_time = 0 # timer that will help with maintaining accurate time whenever the game restarts
+game_time = 0 # used to display total play time for a game
+
+# 'game over' state background settings
+gg_text = bgTextStyle.render('Game Over', False, 'white')
+gg_rect = gg_text.get_rect(center =(400, 80))
+
+def displayTime():
+    global game_time
+    current_time = pygame.time.get_ticks() - start_time
+    game_time = formatTime(current_time)
+    time_surface = timeTextStyle.render(f'{game_time}', False, 'white') # f string method to print int
+    time_rect = time_surface.get_rect(center=(100, 50))
+    pygame.draw.rect(screen, 'slateblue3', time_rect)
+    screen.blit(time_surface, time_rect)
+
+def formatTime(time):
+    seconds = time//1000 # minimum integer division
+    minutes = 0
+    while seconds >= 60: # get number of minutes
+        minutes += 1
+        seconds -= 60
+    
+    # ensure there's always two digits shown on each time interval
+    seconds = str(seconds).zfill(2)
+    minutes = str(minutes).zfill(2)
+    return f'{minutes}:{seconds}'
 
 # Code sampled and derived from
 # https://www.techwithtim.net/tutorials/game-development-with-python/side-scroller-pygame/background/
@@ -26,17 +54,24 @@ def redrawBackground():
     global bgX, bgX2
     screen.blit(background, (bgX,0))
     screen.blit(background, (bgX2, 0))
+
     # draws background colour and shape for a specified rectangle
-    pygame.draw.rect(screen, 'slateblue3', bgText_rect, border_radius = 20)
-    screen.blit(bgText, bgText_rect)
+    displayTime()
+
     # move both images in same direction to simulate scrolling background
     bgX -= 1.1
     bgX2 -= 1.1
+
     # reset the image positions once they reach the end
     if bgX < -DISPLAY_WIDTH:
         bgX = DISPLAY_WIDTH
     if bgX2 < -DISPLAY_WIDTH:
         bgX2 = DISPLAY_WIDTH
+
+toast_defeat_image = pygame.image.load('Graphics/toast_defeat.png').convert_alpha()
+toast_defeat_sprite = spritesheet.SpriteSheet(toast_defeat_image)
+toast_defeat_surface = toast_defeat_sprite.getFrame(0, 64, 64)
+toast_defeat_rect = toast_defeat_surface.get_rect(center = (400, 200))
 
 toast_move_image = pygame.image.load('Graphics/toast_move.png').convert_alpha()
 toast_move_spread = spritesheet.SpriteSheet(toast_move_image)
@@ -52,6 +87,7 @@ egg_move_spread = spritesheet.SpriteSheet(egg_move_image)
 egg_move_01 = egg_move_spread.getFrame(0, 64, 64)
 egg_rect = egg_move_01.get_rect(center = (900, 260))
 
+game_active = True # state of the game (switch from playing and 'game over' screen)
 
 while running: # ensure that program doesn't end unless prompted
     for event in pygame.event.get():
@@ -59,27 +95,48 @@ while running: # ensure that program doesn't end unless prompted
             pygame.quit() # cancel the pygame module
             running = False
             exit() # leave the 'while TRUE' loop securely with sys exit
-        if event.type == pygame.KEYDOWN: # keyboard inputs
-            if event.key == pygame.K_SPACE: # spacebar key
-                if toast_rect.bottom == 380: # ensure only a single jump from ground
-                    toast_gravity = -20 # when pressed, send toast upwards
-    
-    redrawBackground()
-    screen.blit(toast_move_01, toast_rect)
-    screen.blit(egg_move_01, egg_rect)
+        
+        if game_active: # event handler for playing state
+            if event.type == pygame.KEYDOWN: # keyboard inputs
+                if event.key == pygame.K_SPACE: # spacebar key
+                    if toast_rect.bottom == 380: # ensure only a single jump from ground
+                        toast_gravity = -20 # when pressed, send toast upwards
+        
+        else: # event handler for 'game over' state
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                start_time = pygame.time.get_ticks()
+                egg_rect.left = 801
 
-    # simulate jumping with gravity
-    toast_gravity+=1
-    toast_rect.y += toast_gravity
+    if game_active: # 'playing' state
+        redrawBackground()
+        screen.blit(toast_move_01, toast_rect)
+        screen.blit(egg_move_01, egg_rect)
 
-    # movement of egg obstacle
-    egg_rect.left -= 3
-    if egg_rect.left < -100: egg_rect.left = 801
+        # simulate jumping with gravity
+        toast_gravity+=1
+        toast_rect.y += toast_gravity
 
-    # maintain floor for toast player
-    if toast_rect.bottom >= 380: 
-        toast_rect.bottom = 380
-        toast_gravity = 0 # reset gravity, rather than have gravity infinitely increasing
+        # movement of egg obstacle
+        egg_rect.left -= 3
+        if egg_rect.left < -100: egg_rect.left = 801
+
+        # maintain floor for toast player
+        if toast_rect.bottom >= 380: 
+            toast_rect.bottom = 380
+            toast_gravity = 0 # reset gravity, rather than have gravity infinitely increasing
+
+        # check to see obstacle and player collision
+        if egg_rect.colliderect(toast_rect):
+            game_active = False
+        
+    else: # 'game over' state
+        screen.fill('lightslateblue')
+        game_time_surface = timeTextStyle.render(f'Time: {game_time}', False, 'white') 
+        game_time_rect = game_time_surface.get_rect(topleft=(150, 300))
+        screen.blit(gg_text, gg_rect)
+        screen.blit(toast_defeat_surface, toast_defeat_rect)
+        screen.blit(game_time_surface, game_time_rect)
 
     pygame.display.update() # updates the display continuously
     timer.tick(60) # ensure that while loop does not run more than 60 times per second (max frame rate)
